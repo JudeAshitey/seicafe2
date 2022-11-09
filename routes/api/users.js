98bc51cf09c32ff2b18fe596fb296c8c860b80c8
unknown <judeashitey4@gmail.com>
2022-11-09 14:16:37 -0500

    More to come

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// POST /api/users
+router.post('/', usersCtrl.create);
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
+
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+// there's no need to mount express.urlencoded middleware
+// why is that?
+app.use(express.json());
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Check if token and create req.user
+app.use(require('./config/checkToken'));
+
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
