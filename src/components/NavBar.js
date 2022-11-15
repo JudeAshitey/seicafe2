22d0540664447eedbd2db18246221ac19ad58fcc
DELL <judeashitey4@gmail.com>
2022-11-14 21:51:53 -0500

    More Code to come

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import * as userService from "./../utilities/users-service";
+const NavBar = ({ user, setUser }) => {
+  const handleLogOut = () => {
+    // Delegate to the users-service
+    userService.logOut();
+    // Update state will also cause a re-render
+    setUser(null);
+  };
+  return (
+    <nav>
+      <NavLink to="/orders" className="some-class">
+        Order History
+      </NavLink> |{" "}
+      &nbsp; | &nbsp;
+      <NavLink to="/orders/new">New Order</NavLink> {" "}&nbsp;&nbsp;
+      <span>Welcome, {user.name}</span>&nbsp;&nbsp;
+      
+      <NavLink to="" onClick={handleLogOut}>
+        Log Out
+      </NavLink>
+    </nav>
+  );
+};
+export default NavBar;
