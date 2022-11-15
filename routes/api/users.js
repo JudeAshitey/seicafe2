392e36091171eca65f7babebc27f96a355605011
DELL <judeashitey4@gmail.com>
2022-11-14 20:54:16 -0500

    More Code to come

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const ensureLoggedIn = require('./../../config/ensureLoggedIn');
+const user = require('./../../models/user');
 const usersCtrl = require('../../controllers/api/users');
 
 // POST /api/users
@@ -8,3 +10,16 @@ router.post('/', usersCtrl.create);
 router.post('/login', usersCtrl.login);
 
 module.exports = router;
+
+/*
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+
+// POST /api/users
+router.post('/', usersCtrl.create);
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
+
+module.exports = router;
+*/
